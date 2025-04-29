dc5f3cbee56b2a720ec7b39207989766b9a1c902
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2025-04-30 02:45:01 +0530

    fix config

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     MODEL: str = 'gemini-1.5-flash-latest'
     EMBEDDING_MODEL: str = 'models/gemini-embedding-exp-03-07'
     EMBEDDING_DIMENSIONS: int = 768
-    REDIS_HOST: str = 'localhost'
+    # REDIS_HOST: str = 'localhost'
     REDIS_PORT: int = 6379
     REDIS_PORT2: int = 6380
     DOCS_DIR: str = 'data/docs'
